d0075a2e4290cf8aa83bd49515ed8e8ee0d7553e
Daniel <danielamram0@gmail.com>
2017-06-24 23:13:43 +0300

    fixed reading from DB

--- functions/index.js
+++ functions/index.js
@@ -30,37 +30,22 @@ exports.getStatus = functions.https.onRequest((req, res) => {
         res.status(403).send('Forbidden!');
     }
 
-    admin.database().ref('/users').then(snapshot => {
-        console.log(snapshot);
-    });
+    admin.database().ref('/users').once('value', (snapshot => {
+        let statusRes = '';
 
-    // Enable CORS using the `cors` express middleware.
-    cors(req, res, () => {
-        let jungleBook = "The moonlight was blocked out of the mouth of the cave, for Shere Khan's\n" +
-            'great square head and shoulders were thrust into the entrance. Tabaqui,\n' +
-            'behind him, was squeaking: "My lord, my lord, it went in here!"\n' +
-            '\n' +
-            '"Shere Khan does us great honor," said Father Wolf, but his eyes were\n' +
-            'very angry. "What does Shere Khan need?"\n' +
-            '\n' +
-            "\"My quarry. A man's cub went this way,\" said Shere Khan. \"Its parents\n" +
-            'have run off. Give it to me."\n' +
-            '\n' +
-            "Shere Khan had jumped at a woodcutter's campfire, as Father Wolf had\n" +
-            'said, and was furious from the pain of his burned feet. But Father Wolf\n' +
-            'knew that the mouth of the cave was too narrow for a tiger to come in\n' +
-            "by. Even where he was, Shere Khan's shoulders and forepaws were cramped\n" +
-            "for want of room, as a man's would be if he tried to fight in a barrel.\n" +
-            '\n' +
-            '"The Wolves are a free people," said Father Wolf. "They take orders from\n' +
-            "the Head of the Pack, and not from any striped cattle-killer. The man's\n" +
-            'cub is ours--to kill if we choose."';
-
-        QRCode.toDataURL(jungleBook, function (err, url) {
-            if (err) console.log('error: ' + err);
-
-            res.header('Content-Type', 'text/html');
-            res.status(200).send("<!DOCTYPE html/><html><head><title>node-qrcode</title></head><body><img src='" + url + "'/></body></html>")
+        snapshot.forEach((userSnapshot) => {
+            statusRes += userSnapshot.val().displayName + userSnapshot.val().phoneNumber;
         });
-    });
+
+        cors(req, res, () => {
+
+            QRCode.toDataURL(statusRes, function (err, url) {
+                if (err) console.log('error: ' + err);
+
+                res.header('Content-Type', 'text/html');
+                res.status(200).send("<!DOCTYPE html/><html><head><title>node-qrcode</title></head><body><img src='" + url + "'/></body></html>")
+            });
+
+        });
+    }));
 });
